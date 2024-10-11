1f9196b7b4e983382a3e2a8d3704ec24de8f09ab
Dhruv Singh <dhruvsinghs437@gmail.com>
2024-10-11 10:21:41 +0530

    api url fixes

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,29 +1,30 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_BASE_URL;
+console.log(API_URL,"check");
 
 const api = axios.create({
   baseURL: API_URL,
 });
 
 export const registerUser = async (username: string, password: string) => {
-  return await api.post('/register', { username, password });
+  return await api.post(`${API_URL}/register`, { username, password });
 };
 
 export const loginUser = async (username: string, password: string) => {
-  return await api.post('/login', { username, password });
+  return await api.post(`${API_URL}/login`, { username, password });
 };
 
 export const uploadImage = async (file: File, token: string) => {
   const formData = new FormData();
   formData.append('file', file);
-  return await api.post('/upload', formData, {
+  return await api.post(`${API_URL}/upload`, formData, {
     headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'multipart/form-data' },
   });
 };
 
 export const fetchImages = async (token: string) => {
-  const response = await api.get('/images', { // Use the instance instead of axios directly
+  const response = await api.get(`${API_URL}/images`, { // Use the instance instead of axios directly
     headers: { Authorization: `Bearer ${token}` },
   });
   return response;
